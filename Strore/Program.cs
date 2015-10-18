368109737c9755954c563d7d0b814ce23c017cb0
Andy Yang <andy_yang@maxense.com.tw>
2015-10-18 20:48:55 +0800

    init

---
+++ Strore/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Strore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }        
+    }
+
+    public class Sell
+    {
+        public List<int> GetPriceSum(List<int> costs, int numberOfGroup)
+        {
+            var result = new List<int>();
+
+            if (numberOfGroup < 0) numberOfGroup = 1;
+
+            var group = costs.Count % numberOfGroup == 0 ?
+                costs.Count / numberOfGroup : costs.Count / numberOfGroup + 1;
+
+            for (int i = 0; i < group; i++)
+            {
+                int total;
+                if (costs.Count >= numberOfGroup)
+                {
+                    total = costs.Take(numberOfGroup).Sum();
+                    result.Add(total);
+                }
+                else
+                {
+                    total = costs.Sum();
+                    result.Add(total);
+                    break;
+                }
+
+                costs.RemoveRange(0, numberOfGroup);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strore;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetPriceSum_sum_of_cost_group_size_3()
+        {
+            //arrange
+            var target = new Sell();
+            var groupSize = 3;
+            var costs = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            var expected = new List<int> {6, 15, 24, 21};
+            //act
+            var actual = target.GetPriceSum(costs, groupSize);
+            //assert
+            CollectionAssert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void GetPriceSum_sum_of_revenue_group_size_4()
+        {
+            //arrange
+            var target = new Sell();
+            var groupSize = 4;
+            var costs = new List<int> { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
+            var expected = new List<int> { 50, 66, 60 };
+            //act
+            var actual = target.GetPriceSum(costs, groupSize);
+            //assert
+            CollectionAssert.AreEqual(actual, expected);
+
+        }
+    }
+}
